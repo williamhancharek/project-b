5e0575f119565cac09e6d8fb05dd1100b587c0a5
ElviraTsilo <56411952+ElviraTsilo@users.noreply.github.com>
2022-12-21 22:38:38 +0200

    Created the form of simple calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState(0);
+
+  function handleInput(event) {
+    setInput(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(input)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form onSubmit={handleSubmit} className="search-form" id="search-form">
+          <div className="row">
+            <div className="col-3">
+              <div className="form-group form-group-warning">
+                <input type="text" className="form-control" id="number-input" />
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="form-group form-group-warning">
+                <input
+                  type="text"
+                  placeholder="Enter a number"
+                  className="form-control"
+                  id="number-input"
+                  onChange={handleInput}
+                />
+              </div>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-primary">Add</button>
+              <button className="btn btn-primary pl-3">Clear</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
