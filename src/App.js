0777bd38abce1dc36d0b134284298ee5983c4d30
ElviraTsilo <56411952+ElviraTsilo@users.noreply.github.com>
2023-02-20 15:53:59 +0200

    Added number buttons

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ export default function App() {
   const [result, setResult] = useState(0);
   const [rememberResult, setRememberResult] = useState(0);
 
-  function handleInput(event) {
-    setInput(event.target.value);
-  }
-
   function handleSum() {
     let number = Number(input);
     let secondNumber = Number(result);
@@ -29,7 +25,7 @@ export default function App() {
     let secondNumber = Number(result);
     let subtraction = secondNumber - number;
     setResult(subtraction);
-    setInput("");
+    setInput(0);
   }
 
   function handleMult() {
@@ -57,10 +53,14 @@ export default function App() {
     setResult("");
   }
 
+  function handleInput(event) {
+    setInput(event.target.value);
+  }
+
   return (
     <div className="App">
       <div className="container">
-        <h1>Form Calculator</h1>
+        <h1 className="text-center">Calculator</h1>
         <div className="row">
           <div className="col-3">
             <div className="form-group form-group-warning">
@@ -76,68 +76,187 @@ export default function App() {
           <div className="col-3">
             <div className="form-group form-group-warning">
               <input
-                type="number"
-                placeholder="Enter a number"
+                type="text"
                 className="form-control"
                 value={input}
                 id="number-input"
-                onChange={handleInput}
               />
             </div>
           </div>
-          <div className="col-3">
-            <button onClick={handleSum} className="btn btn-primary">
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleSum}
+              className="btn btn-primary"
+            >
               +
             </button>
+          </div>
+          <div className="col-2">
             <button
-              type="submit"
+              type="button"
               onClick={resetInputField}
-              className="btn btn-primary clear pl-3"
+              className="btn btn-primary clear"
             >
               Clear
             </button>
-            <div className="row">
-              <div className="col-2 mt-2">
-                <button
-                  type="submit"
-                  onClick={handleSubtr}
-                  className="btn btn-primary"
-                >
-                  -
-                </button>
-              </div>
-              <div className="col-10 mt-2">
-                <button
-                  type="submit"
-                  onClick={handleMemorization}
-                  className="btn btn-primary"
-                >
-                  Memo
-                </button>
-              </div>
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col-2">
+            <button
+              type="button"
+              value="7"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              7
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="8"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              8
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="9"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              9
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleDivision}
+              className="btn btn-primary"
+            >
+              ÷
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleMemorization}
+              className="btn btn-primary"
+            >
+              Memo
+            </button>
+          </div>
+        </div>
+        <div className="row mt-2">
+          <div className="col-2">
+            <button
+              type="button"
+              value="4"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              4
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="5"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              5
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="6"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              6
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleMult}
+              className="btn btn-primary"
+            >
+              *
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleShowingMemorizedValue}
+              className="btn btn-primary"
+            >
+              DM
+            </button>
+          </div>
+        </div>
+        <div className="row mt-2 pb-4">
+          <div className="col-2">
+            <button
+              type="button"
+              value="1"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              1
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="2"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              2
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              value="3"
+              onClick={handleInput}
+              className="btn btn-primary pl-3"
+            >
+              3
+            </button>
+          </div>
+          <div className="col-2">
+            <button
+              type="button"
+              onClick={handleSubtr}
+              className="btn btn-primary"
+            >
+              -
+            </button>
+          </div>
+          <div className="row mt-2 pb-4">
+            <div className="col-4">
+              <button
+                type="button"
+                value="0"
+                onClick={handleInput}
+                className="btn btn-primary last-row pl-3"
+              >
+                0
+              </button>
             </div>
-            <div className="row">
-              <div className="col-2 mt-2">
-                <button onClick={handleMult} className="btn btn-primary">
-                  *
-                </button>
-              </div>
-              <div className="col-10 mt-2">
-                <button
-                  type="submit"
-                  onClick={handleShowingMemorizedValue}
-                  className="btn btn-primary"
-                >
-                  DM
-                </button>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col mt-2">
-                <button onClick={handleDivision} className="btn btn-primary">
-                  /
-                </button>
-              </div>
+            <div className="col-4">
+              <button type="button" className="btn btn-primary last-row pl-3">
+                =
+              </button>
             </div>
           </div>
         </div>
