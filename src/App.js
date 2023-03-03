c5577538801d73efefb6abf69a1ec21fc67262fd
ElviraTsilo <56411952+ElviraTsilo@users.noreply.github.com>
2023-03-03 14:58:55 +0200

    Added functionality to change the sign and percentage buttons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   const [number, setNumber] = useState("");
-  const [previousNum, setPreviousNum] = useState(0);
+  const [previousNum, setPreviousNum] = useState();
   const [operator, setOperator] = useState();
-  //const [rememberResult, setRememberResult] = useState(0);
 
   function handleInput(event) {
     let input = event.target.value;
@@ -17,33 +16,36 @@ export default function App() {
     let operatorInput = event.target.value;
     setOperator(operatorInput);
     setPreviousNum(number);
-    setNumber(0);
+    setNumber("");
   }
 
   function dealCulculation() {
-    if(operator === "+") {
+    if (operator === "+") {
       setNumber(Number(previousNum) + Number(number));
-    } else if(operator === "÷") {
-      setNumber(Number(previousNum) / Number(number)); 
-    } else if(operator === "*") {
+    } else if (operator === "÷") {
+      setNumber(Number(previousNum) / Number(number));
+    } else if (operator === "*") {
       setNumber(Number(previousNum) * Number(number));
-    } else if(operator === "-") {
+    } else if (operator === "-") {
       setNumber(Number(previousNum) - Number(number));
     }
   }
 
   function resetInputField() {
-    setNumber(0);
+    setNumber("");
   }
-  
-  /*function handleMemorization() {
-    setRememberResult(result);
-  }*/
 
-  /*function handleShowingMemorizedValue() {
-    setNumber(rememberResult);
-    setResult("");
-  }*/
+  function handleSignChanging() {
+    if(number > 0) {
+      setNumber(-number)
+    } else {
+      setNumber(number)
+    }
+  }
+
+  function handlePercentage() {
+    setNumber(number/100);
+  }
 
   return (
     <div className="App">
@@ -61,191 +63,203 @@ export default function App() {
             </div>
           </div>
         </div>
-        <div className="row mt-2">
-          <div className="col-2">
-            <button
-              type="button"
-              value={7}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              7
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={8}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              8
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={9}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              9
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              onClick={handleOperations}
-              value={"+"}
-              className="btn btn-primary"
-            >
-              +
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              //onClick={handleMemorization}
-              className="btn btn-primary"
-            >
-              Memo
-            </button>
-          </div>
-        </div>
-        <div className="row mt-2">
-          <div className="col-2">
-            <button
-              type="button"
-              value={4}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              4
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={5}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              5
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={6}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              6
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={"÷"}
-              onClick={handleOperations}
-              className="btn btn-primary"
-            >
-              ÷
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              //onClick={handleShowingMemorizedValue}
-              className="btn btn-primary"
-            >
-              DM
-            </button>
-          </div>
-        </div>
-        <div className="row mt-2 pb-4">
-          <div className="col-2">
-            <button
-              type="button"
-              value={1}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              1
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={2}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              2
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={3}
-              onClick={handleInput}
-              className="btn btn-primary pl-3"
-            >
-              3
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              value={"*"}
-              onClick={handleOperations}
-              className="btn btn-primary"
-            >
-              *
-            </button>
-          </div>
-          <div className="col-2">
-            <button
-              type="button"
-              onClick={resetInputField}
-              className="btn btn-primary clear"
-            >
-              Clear
-            </button>
-          </div>
-          <div className="row mt-2 pb-4">
-            <div className="col-4">
+        <div className="buttons-container">
+          <div className="row mt-2">
+            <div className="col-2">
               <button
                 type="button"
-                value={0}
+                value={7}
                 onClick={handleInput}
-                className="btn btn-primary last-row pl-3"
+                className="btn btn-primary pl-3"
               >
-                0
-              </button>
-            </div>
-            <div className="col-4">
-              <button
-                type="button"
-                onClick={dealCulculation}
-                className="btn btn-primary last-row pl-3"
-              >
-                =
+                7
               </button>
             </div>
             <div className="col-2">
               <button
                 type="button"
-                value={"-"}
+                value={8}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                8
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={9}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                9
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
                 onClick={handleOperations}
+                value={"+"}
                 className="btn btn-primary"
               >
-                -
+                +
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                onClick={resetInputField}
+                className="btn operations"
+              >
+                Clear
               </button>
             </div>
           </div>
+          <div className="row mt-2">
+            <div className="col-2">
+              <button
+                type="button"
+                value={4}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                4
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={5}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                5
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={6}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                6
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={"÷"}
+                onClick={handleOperations}
+                className="btn btn-primary"
+              >
+                ÷
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                onClick={handleSignChanging}
+                className="btn operations"
+              >
+                +/-
+              </button>
+            </div>
+          </div>
+          <div className="row mt-2">
+            <div className="col-2">
+              <button
+                type="button"
+                value={1}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                1
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={2}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                2
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={3}
+                onClick={handleInput}
+                className="btn btn-primary pl-3"
+              >
+                3
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                value={"*"}
+                onClick={handleOperations}
+                className="btn btn-primary"
+              >
+                *
+              </button>
+            </div>
+            <div className="col-2">
+              <button
+                type="button"
+                onClick={handlePercentage}
+                className="btn operations"
+              >
+                %
+              </button>
+            </div>
+            </div>
+            <div className="row mt-2">
+              <div className="col-2">
+                <button
+                  type="button"
+                  value={0}
+                  onClick={handleInput}
+                  className="btn btn-primary"
+                >
+                  0
+                </button>
+              </div>
+              <div className="col-2">
+                <button
+                  type="button"
+                  value={"."}
+                  onClick={handleInput}
+                  className="btn btn-primary"
+                >
+                  .
+                </button>
+              </div>
+              <div className="col-2">
+                <button
+                  type="button"
+                  onClick={dealCulculation}
+                  className="btn btn-primary"
+                >
+                  =
+                </button>
+              </div>
+              <div className="col-2">
+                <button
+                  type="button"
+                  value={"-"}
+                  onClick={handleOperations}
+                  className="btn btn-primary"
+                >
+                  -
+                </button>
+              </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
   );
 }
